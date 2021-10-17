41c46b792aa3d745bd195c90700a2af97daae459
Daniel Paxtian <daniel.paxtian98@hotmail.com>
2021-10-17 12:04:49 -0500

    Actualizacion

--- PRA05-Ruta/src/HospitalViewController.java
+++ PRA05-Ruta/src/HospitalViewController.java
@@ -1,8 +1,11 @@
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
@@ -12,9 +15,9 @@ import javafx.scene.control.ToggleGroup;
 public class HospitalViewController implements Initializable {
 
     @FXML
-    private ComboBox<?> cbGenero;
+    private ComboBox<String> cbGenero;
     @FXML
-    private ComboBox<?> cbEdad;
+    private TextField cbEdad;
     @FXML
     private TextField tfAltura;
     @FXML
@@ -33,13 +36,47 @@ public class HospitalViewController implements Initializable {
     private RadioButton rbGrado5;
     @FXML
     private Label lbResultado;
+    
+    private int edad, estatura, peso;
+    private String genero;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<String> generos = FXCollections.observableArrayList();
+        generos.addAll("Femenino","Masculino");
+        this.cbGenero.setItems(generos);
     }    
 
     @FXML
     private void calcularCalorias(ActionEvent event) {
+        validarDatos();
+    }
+    
+    private void validarDatos(){
+        try{
+            this.edad = Integer.parseInt(this.cbEdad.getText());
+            this.estatura = Integer.parseInt(this.tfAltura.getText());
+            this.peso = Integer.parseInt(this.tfPeso.getText());
+            
+            if(edad < 0 || edad > 120){
+                System.out.println("Valores de edad permitidos entre 0 y 120");
+            }
+            if(this.estatura < 20 || this.estatura > 250){
+                System.out.println("Error en la estatura");
+            }
+            if(this.peso < 0.25 || this.peso > 600){
+                System.out.println("error en el peso");
+            }
+            if(rbGrado1.isSelected() == false && rbGrado2.isSelected() == false && rbGrado3.isSelected() == false && rbGrado4.isSelected() == false && rbGrado5.isSelected() == false){
+                System.out.println("No selecciono ningun nivel de actividad");
+            }
+        }catch(NumberFormatException ex){
+            Alert alertEmptyInfo = new Alert(Alert.AlertType.WARNING);
+            alertEmptyInfo.setTitle("Advertencia");
+            alertEmptyInfo.setHeaderText("");
+            alertEmptyInfo.setContentText("Ingrese unicamente números");
+            alertEmptyInfo.showAndWait();
+        }
     }
     
 }
