f9fdc0d9e5fa87210e877a14559370045de1e772
Daniel Paxtian <daniel.paxtian98@hotmail.com>
2021-10-16 19:03:51 -0500

    Actualizacion inicial para ejecutar el proyecto

--- PRA05-Ruta/src/Main.java
+++ PRA05-Ruta/src/Main.java
@@ -1,33 +1,25 @@
+import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("HospitalView.fxml"));
+            Pane ventana = (Pane) loader.load();
+
+            Scene scene = new Scene(ventana);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     public static void main(String[] args) {
